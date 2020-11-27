b8bbdd4bf392264a8ac6d15eefe903246484c15b
Alexey Novichikhin <novichikhin.au@gmail.com>
2020-11-27 15:19:15 +0200

    add logs for accept invite

--- src/KeyKeeperApi/Grpc/InvitesService.cs
+++ src/KeyKeeperApi/Grpc/InvitesService.cs
@@ -113,7 +113,7 @@ namespace KeyKeeperApi.Grpc
             validatorLinkEntity.PushNotificationFcmToken = request.PushNotificationFCMToken;
             await _validatorLinkWriter.InsertOrReplaceAsync(validatorLinkEntity);
 
-            _logger.LogInformation("Invitation accepted. InviteId='{InviteId}'; ValidatorId='{ValidatorId}'", request.InviteId, request.ValidatorId);
+            _logger.LogInformation("Invitation accepted. InviteId='{InviteId}'; ValidatorId='{ValidatorId}'; PushNotificationFcmToken='{PushNotificationFcmToken}'", request.InviteId, request.ValidatorId, request.PushNotificationFCMToken);
 
             return resp;
         }
