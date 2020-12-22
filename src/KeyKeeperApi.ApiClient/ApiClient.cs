5e324b4e77d2e9c3b9802f3ec1ebda0cc5c9ea25
Aleksandr Belousov <aleksandr.belousov@swisschain.io>
2020-12-22 17:51:08 +0400

    Create client lib

---
+++ src/KeyKeeperApi.ApiClient/ApiClient.cs
@@ -0,0 +1,22 @@
+﻿using Swisschain.KeyKeeperApi.ApiClient.Common;
+using Swisschain.Sirius.GuardianValidatorApi;
+using Swisschain.Sirius.ValidatorApi;
+
+namespace Swisschain.KeyKeeperApi.ApiClient
+{
+    public class ApiClient : BaseGrpcClient, IApiClient
+    {
+        public ApiClient(string serverGrpcUrl, string apiKey) : base(serverGrpcUrl, apiKey)
+        {
+            ValidatorsClient = new Validators.ValidatorsClient(CallInvoker);
+            TransfersClient = new Transfers.TransfersClient(CallInvoker);
+            InvitesClient = new Invites.InvitesClient(CallInvoker);
+            VersionClient = new Version.VersionClient(CallInvoker);
+        }
+
+        public Validators.ValidatorsClient ValidatorsClient { get; }
+        public Transfers.TransfersClient TransfersClient { get; }
+        public Invites.InvitesClient InvitesClient { get; }
+        public Version.VersionClient VersionClient { get; }
+    }
+}
